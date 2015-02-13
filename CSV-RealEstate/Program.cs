5d70038cb3271e754fccd368b4436963d6983a23
PatrickSHYee <patrickshyee@gmail.com>
2015-02-13 07:05:52 -0700

    Got to work
    There was a problem where I set the lot, but all the types were
    something else and was being reset to that type.  Just had to move an if
    statement around.

--- CSV-RealEstate/Program.cs
+++ CSV-RealEstate/Program.cs
@@ -20,25 +20,32 @@ namespace CSV_RealEstate
             
             //Display the average square footage of a Condo sold in the city of Sacramento, 
             //Use the GetAverageSquareFootageByRealEstateTypeAndCity() function.
-            Console.WriteLine("Average of {0} at {1} is {3}", "Condo", "Sacramento", GetAverageSquareFootageByRealEstateTypeAndCity(realEstateSaleList, RealEstateType.Condo, "Sacramento"));
+            Console.WriteLine("Average of {0} at {1} is {2}", "Condo", "Sacramento", GetAverageSquareFootageByRealEstateTypeAndCity(realEstateSaleList, RealEstateType.Condo, "Sacramento"));
 
             //Display the total sales of all residential homes in Elk Grove.  Use the GetTotalSalesByRealEstateTypeAndCity() function for testing.
+            Console.WriteLine("The total sales of all {0} in {1} is {2:C}.", "residential", "Elk Grove", GetTotalSalesByRealEstateTypeAndCity(realEstateSaleList, RealEstateType.Residential, "Elk Grove"));
 
             //Display the total number of residential homes sold in the zip code 95842.  Use the GetNumberOfSalesByRealEstateTypeAndZip() function for testing.
+            Console.WriteLine("The total number of residential homes sold in the zip code 95842 is {0}", GetNumberOfSalesByRealEstateTypeAndZip(realEstateSaleList, RealEstateType.Residential, "95842"));
 
             //Display the average sale price of a lot in Sacramento.  Use the GetAverageSalePriceByRealEstateTypeAndCity() function for testing.
+            Console.WriteLine("The average sale price of a lot in Sacramento is {0}.", GetAverageSalePriceByRealEstateTypeAndCity(realEstateSaleList, RealEstateType.Lot, "sacramento"));
 
             //Display the average price per square foot for a condo in Sacramento. Round to 2 decimal places. Use the GetAveragePricePerSquareFootByRealEstateTypeAndCity() function for testing.
+            Console.WriteLine("Te average price per square footage for a condo in Sacramento is {0}.", GetAverageSquareFootageByRealEstateTypeAndCity(realEstateSaleList, RealEstateType.Condo, "Sacramento"));
 
             //Display the number of all sales that were completed on a Wednesday.  Use the GetNumberOfSalesByDayOfWeek() function for testing.
+            Console.WriteLine("The number of all sales that were sold on a Wednesday is {0}.", GetNumberOfSalesByDayOfWeek(realEstateSaleList, DayOfWeek.Wednesday));
 
 
             //Display the average number of bedrooms for a residential home in Sacramento when the 
             // price is greater than 300000.  Round to 2 decimal places.  Use the GetAverageBedsByRealEstateTypeAndCityHigherThanPrice() function for testing.
+            Console.WriteLine("The average number of bedrooms for a residential home in Sacramento when the price is greater than $300,000 is {0}.", GetAverageBedsByRealEstateTypeAndCityHigherThanPrice(realEstateSaleList, RealEstateType.Residential, "Sacramento", 300000m));
 
             //Extra Credit:
             //Display top 5 cities by the number of homes sold (using the GroupBy extension)
             // Use the GetTop5CitiesByNumberOfHomesSold() function for testing.
+            Console.WriteLine("Top 5 cities by the number of homes sold is {0}", String.Join("\n", GetTop5CitiesByNumberOfHomesSold(realEstateSaleList)));
 
         }
 
@@ -67,45 +74,92 @@ namespace CSV_RealEstate
         /// <returns>The Average of the square footage</returns>
         public static double GetAverageSquareFootageByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city) 
         {
-            return realEstateDataList.Where(x => x.ReType == realEstateType && x.City.ToLower() == city.ToLower()).Average(x => x.Sqft);
+            return Math.Round(realEstateDataList.Where(x => x.ReType == realEstateType && x.City.ToLower() == city.ToLower()).Average(x => x.Sqft), 2);
         }
 
+        /// <summary>
+        /// Finds the total sales of the real estate type and the city
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <param name="realEstateType">the real estate type</param>
+        /// <param name="city">the city to search</param>
+        /// <returns>the total sales</returns>
         public static decimal GetTotalSalesByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
         {
             return (decimal)realEstateDataList.Where(x => x.ReType == realEstateType && x.City.ToLower() == city.ToLower()).Sum(x=>x.Price);
         }
 
+        /// <summary>
+        /// The number of sales of the real estate type and the zip code
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <param name="realEstateType">the real estate type</param>
+        /// <param name="zipcode">the zip code to search</param>
+        /// <returns>the number of sales</returns>
         public static int GetNumberOfSalesByRealEstateTypeAndZip(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string zipcode)
         {
-            return 0;
+            return realEstateDataList.Count(x=>x.ReType == realEstateType && x.Zip.ToString() == zipcode);
         }
 
-        
+        /// <summary>
+        /// The average of sale price based on the type of real estate and the city.
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <param name="realEstateType">the real estate type</param>
+        /// <param name="city">the city to search</param>
+        /// <returns>the average of sale price</returns>
         public static decimal GetAverageSalePriceByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
         {
-            //Must round to 2 decimal points
-            return 0.0m;
+            //Must round to 2 decimal points  && x.City.ToLower() == city.ToLower() x.ReType == realEstateType
+            return (decimal)Math.Round(realEstateDataList.Where(x => x.ReType == realEstateType && x.City.ToLower() == city.ToLower()).Average(x => x.Price), 2);
         }
+
+        /// <summary>
+        /// The average price per square footage based on real estate type and city
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <param name="realEstateType">type of real estate</param>
+        /// <param name="city">the city of search</param>
+        /// <returns>the average price per square footage</returns>
         public static decimal GetAveragePricePerSquareFootByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
         {
-            //Must round to 2 decimal points
-            return 0.0m;
+            //Must round to 2 decimal point
+            return (decimal) Math.Round(realEstateDataList.Where(x=>x.ReType == realEstateType && x.City.ToLower() == city.ToLower()).Average(x=>x.Price/x.Sqft), 2);
         }
 
+        /// <summary>
+        /// the number of sales on a given day of the week
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <param name="dayOfWeek">day of the week to search for</param>
+        /// <returns>the number of sales</returns>
         public static int GetNumberOfSalesByDayOfWeek(List<RealEstateSale> realEstateDataList, DayOfWeek dayOfWeek)
         {
-            return 0;
+            return realEstateDataList.Count(x=>x.Sale_date.DayOfWeek == dayOfWeek);
         }
 
+        /// <summary>
+        /// The average amount of beds in a city based on the type for a price range.
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <param name="realEstateType">the type of real estate</param>
+        /// <param name="city">in a city</param>
+        /// <param name="price">the price</param>
+        /// <returns>the amount of beds</returns>
         public static double GetAverageBedsByRealEstateTypeAndCityHigherThanPrice(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city, decimal price)
         {
             //Must round to 2 decimal points
-            return 0.0;
+            return Math.Round(realEstateDataList.Where(x=>x.ReType == realEstateType && x.City.ToLower() == city.ToLower() && (decimal)x.Price > price).Average(x=>x.Beds), 2);
         }
 
+        /// <summary>
+        /// top 5 sellers in a city.
+        /// </summary>
+        /// <param name="realEstateDataList">the listing</param>
+        /// <returns>the top 5 cities</returns>
         public static List<string> GetTop5CitiesByNumberOfHomesSold(List<RealEstateSale> realEstateDataList)
         {
-            return new List<string>();
+            return realEstateDataList.GroupBy(x=>x.City).OrderByDescending(x=>x.Count()).Take(5).Select(x=>x.Key).ToList();
         }
     }
 
@@ -125,29 +179,29 @@ namespace CSV_RealEstate
     {
         //Create properties, using the correct data types (not all are strings) for all columns of the CSV
         private string _street;
-        public string Street { get; set; }
+        public string Street { get{return _street;} set { _street = value; } }
         private string _city;
-        public string City { get; set; }
+        public string City { get{return _city;} set { _city = value; } }
         private string _state;
-        public string State { get; set; }
+        public string State { get { return _state; } set { _state = value; } }
         private int _zip;
-        public int Zip { get; set; }
+        public int Zip { get { return _zip; } set { _zip = value; } }
         private int _beds;
-        public int Beds { get; set; }
+        public int Beds { get { return _beds; } set { _beds = value; } }
         private int _baths;
-        public int Baths { get; set; }
+        public int Baths { get { return _baths; } set { _baths = value; } }
         private double _sqft;
-        public double Sqft { get; set; }
+        public double Sqft { get { return _sqft; } set { _sqft = value; } }
         private RealEstateType _reType;
-        public RealEstateType ReType { get; set;}
-        private string _sale_date;
-        public string Sale_date { get; set; }
-        private int _price;
-        public int Price { get; set; }
+        public RealEstateType ReType { get { return _reType; } set { _reType = value; } }
+        private DateTime _sale_date;
+        public DateTime Sale_date { get { return _sale_date; } set { _sale_date = value; } }
+        private double _price;
+        public double Price { get { return _price; } set { _price = value; } }
         private double _latitude;
-        public double Latitude { get; set; }
+        public double Latitude { get { return _latitude; } set { _latitude = value; } }
         private double _longitude;
-        public double Longitude { get; set; }
+        public double Longitude { get { return _longitude; } set { _longitude = value; } }
 
         //The constructor will take a single string arguement.  This string will be one line of the real estate data.
         // Inside the constructor, you will seperate the values into their corrosponding properties, and do the necessary conversions
@@ -163,10 +217,6 @@ namespace CSV_RealEstate
             this.Beds = int.Parse(data[4]);
             this.Baths = int.Parse(data[5]);
             this.Sqft = double.Parse(data[6]);
-            if (this.Sqft == 0)
-            {
-                this.ReType = RealEstateType.Lot;
-            }
             if (data[7] == "Multi-Family")
             {
                 this.ReType = RealEstateType.MultiFamily;
@@ -179,8 +229,12 @@ namespace CSV_RealEstate
             {
                 this.ReType = RealEstateType.Residential;
             }
-            this.Sale_date = data[8];
-            this.Price = int.Parse(data[9]);
+            if (this.Sqft == 0)
+            {
+                this.ReType = RealEstateType.Lot;
+            }
+            this.Sale_date = DateTime.Parse(data[8]);
+            this.Price = double.Parse(data[9]);
             this.Latitude = double.Parse(data[10]);
             this.Longitude = double.Parse(data[11]);
         }
